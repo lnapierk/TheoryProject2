dccf13b6bb3ca2e0ffcd34903e37962e47b5fd34
Luke Napierkowski <lnapierk@nd.edu>
2017-11-02 04:38:29 -0400

    message

--- nfa2dfa-teamrocket.py
+++ nfa2dfa-teamrocket.py
@@ -1,6 +1,13 @@
 import sys
 
-
+def makeESetList(anNfa, astate, thelist):
+    retList = thelist#this will be the returned list
+    if '~' in anNfa[astate]:#if there is an epsilon
+        for nstate in anNfa[astate]['~']:#for each state reached by epsilon
+            if nstate not in retList:#if it is not already in the list
+                retList.append(nstate)#add it to the list
+                retList = makeESetList(anNfa, nstate, list(set(thelist+retList)))#check for its epsilons
+    return retList #return the list
 
 theNfa = {}#create dictionary to represent NFA
 f = open(sys.argv[1], "r") #open file
@@ -12,9 +19,13 @@ acceptingStates = f.readline().strip().split(',')#get accepting states
 for state in stateListNfa:
     theNfa[state] = {}#initializes all state keys to have values of empty dictionaries
 for line in f:
-    line = line.strip().split()
+    line = line.strip().split(',')
     if line[1] not in theNfa[line[0]]:#if the input char is not in the state yet
         theNfa[line[0]][line[1]] = [line[2]]#create a list for its resultant state
     else:
         theNfa[line[0]][line[1]].append(line[2])#or append the possible result to the list
-print(theNfa)
+
+esets = {} # initialize dictionary for the epsilon set things
+for state in stateListNfa:
+    strtList = [state] # all esets contain the state itself
+    esets[state] = makeESetList(theNfa, state, strtList)#map a state to the set it can reach
